bb5d0c6d79b6ec907749cc12c2e417364c88fad7
Noah van Boven <noahvanboven@gmail.com>
2019-12-01 16:24:28 +0100

    Adding dat 1

---
+++ day1/day1.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+
+function part1() {
+    let sum = 0;
+    for (const mass of input) {
+        const fuel = Math.floor(mass/3) - 2;
+        sum += fuel;
+    }
+    return sum;
+}
+
+function part2() {
+    let sum = 0;
+    for (const mass of input) {
+        let fuel = Math.floor(mass/3) - 2;
+        while (fuel > 0) {
+            sum += fuel;
+            fuel = Math.floor(fuel/3) - 2;
+        }
+    }
+    return sum;
+}
+
+console.log(part1());
+console.log(part2());
