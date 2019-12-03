86c7a7205469200dde0232f78aeb9c3cdfd4dd3a
Nola1222 <noahvanboven@gmail.com>
2019-12-03 08:49:21 +0000

    Adding day 2

---
+++ day2/day2.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt', 'utf8').split(',').map(Number);
+
+function day1() {
+    const opcodes = [...input];
+    opcodes[1] = 12;
+    opcodes[2] = 2;
+    for (let i = 0; i < opcodes.length; i += 4) {
+        if (opcodes[i] === 1) {
+            opcodes[opcodes[i+3]] = opcodes[opcodes[i+1]] + opcodes[opcodes[i+2]];
+        } else if (opcodes[i] === 2) {
+            opcodes[opcodes[i+3]] = opcodes[opcodes[i+1]] * opcodes[opcodes[i+2]];
+        } else if (opcodes[i] === 99) {
+            break;
+        }
+    }
+    return opcodes[0];
+}
+
+function day2() {
+    const expectedOpcode = 19690720;
+    for (let noun = 0; noun <= 99; noun++) {
+        for (let verb = 0; verb <= 99; verb++) {
+            const opcodes = [...input];
+            opcodes[1] = noun;
+            opcodes[2] = verb;
+            for (let i = 0; i < opcodes.length; i += 4) {
+                if (opcodes[i] === 1) {
+                    opcodes[opcodes[i+3]] = opcodes[opcodes[i+1]] + opcodes[opcodes[i+2]];
+                } else if (opcodes[i] === 2) {
+                    opcodes[opcodes[i+3]] = opcodes[opcodes[i+1]] * opcodes[opcodes[i+2]];
+                } else if (opcodes[i] === 99) {
+                    break;
+                }
+            }
+            if (opcodes[0] === expectedOpcode) {
+                return 100 * noun + verb;
+            }
+        }
+    }
+}
+
+console.log(day1());
+console.log(day2());
